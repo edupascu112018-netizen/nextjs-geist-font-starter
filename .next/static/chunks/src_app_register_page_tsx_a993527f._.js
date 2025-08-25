6ca48157330d3ea6eb01088585c36b39470e6cb5
edupascu112018-netizen <edupascu112018@gmail.com>
2025-08-25 12:44:27 -0400

    Add .next/static/chunks/src_app_register_page_tsx_a993527f._.js 1756140266677

---
+++ .next/static/chunks/src_app_register_page_tsx_a993527f._.js
@@ -0,0 +1,8 @@
+(globalThis.TURBOPACK_CHUNK_LISTS = globalThis.TURBOPACK_CHUNK_LISTS || []).push({
+    script: typeof document === "object" ? document.currentScript : undefined,
+    chunks: [
+  "static/chunks/node_modules_5a8c83c3._.js",
+  "static/chunks/src_fdbad7e0._.js"
+],
+    source: "dynamic"
+});
